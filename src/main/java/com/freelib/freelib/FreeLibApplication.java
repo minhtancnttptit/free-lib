f376908bddf0823ccef754a3224ec96b9bf14881
minhtancnttptit <minhtancnttptit@gmail.com>
2020-05-12 18:45:11 +0700

    first commit

---
+++ src/main/java/com/freelib/freelib/FreeLibApplication.java
@@ -0,0 +1,13 @@
+package com.freelib.freelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeLibApplication.class, args);
+    }
+
+}
